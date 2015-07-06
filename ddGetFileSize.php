a6aaf350285ff4539c246ac366ed29cb7b0d43d6
Ilyas Ronef <ilyas@divandesign.biz>
2015-07-06 17:50:39 +0500

    Version 1.2: + Если файл получить не удалось, пробуем отрезать '/' от начала имени. + Добавлен плейсхолдер «[+ext+]», в который записывается расширение файла при выводе через шаблон.

--- ddGetFileSize.php
+++ ddGetFileSize.php
@@ -1,13 +1,15 @@
 <?php
-//ddGetFileSize.php v1.0
-//Выводит размер файла
-//file - имя файла
-//tpl -  шаблон для вывода.
-//type - тип вывода размера файла. Доступные значения: -1, 0, 1, 2. По умолчанию - 0.
-//prec - количество цифр после запятой. По умолчанию - 2.
+/** ddGetFileSize.php v1.2 (2011-04-20)
+ * Выводит размер файла
+ * file - имя файла.
+ * tpl -  шаблон для вывода, без шаблона возвращает размер. Плейсхолдеры: размер файла — filesize, расширение файла — ext.
+ * type - тип вывода размера файла. Доступные значения: -1, 0, 1, 2. По умолчанию - 0.
+ * prec - количество цифр после запятой. По умолчанию - 2.
+ * Сниппет разработан студией Диван.Дизайн (www.divandesign.ru)
+ */
 
-//Сниппет разработан студией Диван.Дизайн (www.divandesign.ru)
-
+//Проверяем на существование файла
+if (!file_exists($file)) $file = ltrim($file, "/");
 if (isset($file) && $file != "" && file_exists($file)){
 	$type = isset($type) ? intval($type) : 0;
 	$prec = isset($prec) ? intval($prec) : 2;
@@ -34,11 +36,12 @@ if (isset($file) && $file != "" && file_exists($file)){
 			return round($size,$prec).$mas[$i];
 		}
 	}
-
+	//Формируем строку размера файла
 	$result = ddfsize_format(filesize($file), $type, $prec);
-
+	//Если есть tpl, то парсим или возвращаем размер
 	if (isset($tpl)){
-		$result = $modx->parseChunk($tpl,array('filesize' => $result),'[+','+]');
+		$ext = substr($file, strrpos($file, '.')+1);//Копируем расширение файла
+		$result = $modx->parseChunk($tpl,array('filesize' => $result, 'ext' => $ext),'[+','+]');
 	}
 
 	return $result;
